08f56740087da2f2dd1275f321fa9c1cadeeeda0
Emily Burns <emily5001@gmail.com>
2018-05-22 15:28:22 +0100

    created tasks path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get "tasks", to: "tasks#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  #get "tasks/new", to: "tasks#new"
+  #post "tasks", to: "tasks#create"
 
 end
