80c76e3e98a59cb3db6bc2256ad5c9b86a3d7a8b
Emily Burns <emily5001@gmail.com>
2018-05-22 17:09:07 +0100

    added the form and the extra html

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+   before_action :set_task, only: [ :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
 
@@ -17,10 +19,27 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+   def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+
   private
 
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   def task_params
-    params.require(:task).permit(:title, :description, :deadline, :priority)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
-  get "tasks", to: "tasks#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "tasks/:id", to: "tasks#show", as: :task
 
-  #get "tasks/new", to: "tasks#new"
-  #post "tasks", to: "tasks#create"
+  # # read all the task
+  get 'tasks', to: 'tasks#index'
+
+  # # add a new task to the app: retrieve info from form and feed it to the index
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # # read one task
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  # # update the info of a given task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # # delete a task
+  delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 
 end
